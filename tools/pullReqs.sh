7f1a50e413488ebe327703b5c1018629759a3826
Pekko Metsä <pekko.metsa@helsinki.fi>
2016-12-16 15:55:06 +0200

    Added pullReqs script, which pull the required roles according to requirements.yml.  Also getting ready for local ansible config (ie. config e.g. in different git repo).

---
+++ tools/pullReqs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+
+# Install all the ansible roles that we depend on
+/usr/bin/ansible-galaxy -p ./roles install -r requirements.yml --force
+
+# Update requirements_mirror.yml for ansible-pull-script 
+# sync group_ and host_vars to the install node
+# update ansible-pull-script on the install node
+# send a flowdock notification to fgci admins
+
+#ansible-playbook install.yml --tags=fgci-install,flowdock --diff -e flowdock_from_address=pullReqs@{{siteDomain}}
